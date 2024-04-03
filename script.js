634122a340f93a2180b8557240457a13b389bce6
Jean <jmoncayo@pursuit.org>
2024-04-03 18:32:53 -0400

    Dynamic Square Generation Complete

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// makes nested div
 document.getElementById("enterBtn").addEventListener("click", function() {
     var parentSquare = document.querySelector(".box");
     var newSquare = document.createElement("div");
     newSquare.classList.add("nested-square");
-    // puts next div generated inside if there an existing square
+
+    // sets random color
+    var randomColor = generateRandomColor();
+    newSquare.style.backgroundColor = randomColor;
+
+    // appends the new square last created square or parent square if there are no existing squares
     var existingSquares = document.querySelectorAll(".nested-square");
     if (existingSquares.length > 0) {
         var lastSquare = existingSquares[existingSquares.length - 1];
@@ -12,3 +16,11 @@ document.getElementById("enterBtn").addEventListener("click", function() {
         parentSquare.appendChild(newSquare);
     }
 });
+
+//generates color
+function generateRandomColor() {
+    var randomHue = Math.floor(Math.random() * 360);
+    var randomSaturation = Math.floor(Math.random() * 100);
+    var randomLightness = Math.floor(Math.random() * 50) + 30; //keeps brighter
+    return "hsl(" + randomHue + ", " + randomSaturation + "%, " + randomLightness + "%)";
+}
